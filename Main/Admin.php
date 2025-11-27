5cfe316e3d330336b444712a8a7e9241d567e393
Andrei Ochangco <196190939+AndreiOchangco@users.noreply.github.com>
2025-11-27 18:23:45 +0800

    e

--- Main/Admin.php
+++ Main/Admin.php
@@ -69,18 +69,10 @@ $username = $_SESSION["username"] ?? "Admin";
                 </a>
             </nav>
 
-<<<<<<< HEAD
             <!-- LOGOUT BUTTON -->
             <div class="admin-bottom-bar">
                 <a href="Logout.php" class="admin-logout">LOGOUT</a>
             </div>
-=======
-        <!-- LOGOUT BUTTON -->
-        <div class="admin-bottom-bar">
-            <a href="Logout.php" class="admin-logout">LOGOUT</a>
-        </div>
-
->>>>>>> 459693ba33b606948b94ad09a79d5b27ddbcf2b4
         </aside>
 
         <!-- MAIN CONTENT AREA -->
